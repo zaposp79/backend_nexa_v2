e3687d41b52e6a2cfb14f5899b1cad71a5a75942
Brayan Ramirez <brayan.david.ramirez@accenture.com>
2026-09-08 15:58:35 -0500

    Space for deploy

--- modules/vision_pyg/helpers/screen_mapper.py
+++ modules/vision_pyg/helpers/screen_mapper.py
@@ -528,7 +528,7 @@ def _build_from_v2_result(
         totales["utilidad"]["porcentaje_contribucion"]  = _contribucion_total / _ingreso_neto_total
         totales["utilidad"]["porcentaje_utilidad_neta"] = _utilidad_neta_total / _ingreso_neto_total
         
-    if(servicio == "cobranzas"):
+    if(servicio == "cobranzas"): 
          totales["utilidad"]["porcentaje_contribucion"] =  totales["utilidad"]["porcentaje_contribucion"] * 100
          totales["utilidad"]["porcentaje_utilidad_neta"] =  totales["utilidad"]["porcentaje_utilidad_neta"] * 100
          
